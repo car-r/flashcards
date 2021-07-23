f3e346adc5c0ac0c30beed06c312563f0412586a
car-r <carson.ri@protonmail.com>
2021-07-22 22:30:21 -0700

    added media query for app container to center card grid above 1000px

--- src/components/flashcardlist/FlashcardListElements.js
+++ src/components/flashcardlist/FlashcardListElements.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const FlashcardListWrapper = styled.div`
     display: grid;
     align-items: center;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
     gap: 1.5em;
 `
